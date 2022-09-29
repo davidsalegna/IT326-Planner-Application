9033d94a6234e80de7f3de5f17eaaa5b15da8e18
dasalegna <98178004+davidsalegna@users.noreply.github.com>
2022-09-29 14:14:46 -0500

    Add project files.

---
+++ IT 326 - Planner Application/Program.cs
@@ -0,0 +1,13 @@
+using IT_326___Planner_Application;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
