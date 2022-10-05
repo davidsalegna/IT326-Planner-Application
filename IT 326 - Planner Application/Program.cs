97c296a377fc5cb3d871676ec26622d55f82f53d
dasalegna <98178004+davidsalegna@users.noreply.github.com>
2022-10-05 11:18:54 -0500

    calendar/event baseline

--- IT 326 - Planner Application/Program.cs
+++ IT 326 - Planner Application/Program.cs
@@ -2,6 +2,7 @@ using IT_326___Planner_Application;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddScoped<DialogService>();
 
 await builder.Build().RunAsync();
