a322cb1b669ffb156bc4c5e1a209056afa4c048a
dasalegna <98178004+davidsalegna@users.noreply.github.com>
2022-10-20 13:38:05 -0500

    worked on framework for event class, it's getting there

---
+++ IT 326 - Planner Application/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using Radzen.Blazor.Rendering;
+
+namespace IT_326___Planner_Application.Models
+{
+    public class Event : object
+    {
+        //Gets or sets the start of the event.
+        public DateTime Start { get; set; }
+
+        //Gets or sets the end of the event.
+        public DateTime End { get; set; }
+
+        //Gets or sets the text of the event.
+        public string? Text { get; set; }
+
+        //Gets or sets the data associated with the event.
+        //public object Data { get; set; }
+    }
+}
+
