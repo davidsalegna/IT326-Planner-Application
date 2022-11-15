c741691393915670e0aef1d497f1aa4c0c84d268
trevormurphyy <trevormurphy72@gmail.com>
2022-11-14 19:01:52 -0600

    Notepad.razor partially completed.

---
+++ IT 326 - Planner Application/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using Radzen.Blazor.Rendering;
+
+namespace IT_326___Planner_Application.Models
+{
+    public class Note : object
+    {
+
+        //Gets or sets the text of the note.
+        public string? Text { get; set; }
+
+    }
+
+}
+
