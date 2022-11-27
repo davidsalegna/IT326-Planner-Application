b4a2fc28392a05754489e052ee8a9e3ae4c397a2
maxglover <maxglover62@gmail.com>
2022-11-27 16:55:19 -0600

    assignments updates

---
+++ IT 326 - Planner Application/Models/Assignment.cs
@@ -0,0 +1,16 @@
+namespace IT_326___Planner_Application.Models{
+
+
+public class Assignment : Object
+{
+    public string Title {get;set;}
+
+    public string Description {get;set;}
+
+    public bool IsDone { get; set; }
+
+    public DateTime DueDate {get;set;}
+
+    public Course course {get;set;}
+}
+}
