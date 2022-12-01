715ce48ae7bfad981bc9acbd8d07399f392b5d25
dasalegna <98178004+davidsalegna@users.noreply.github.com>
2022-11-30 20:36:30 -0600

    added more unit tests

---
+++ PlannerTesting/DeleteCourse.cs
@@ -0,0 +1,20 @@
+﻿using Bunit;
+using IT_326___Planner_Application.Models;
+using IT_326___Planner_Application.Pages;
+using Xunit.Sdk;
+
+namespace PlannerTesting
+{
+    public class DeleteCourse : TestContext
+    {
+        [Fact]
+        public void RemoveCourse()
+        {
+            Course testCourse = Course.CreateCourse(DateTime.Today, DateTime.Today, "Course to Remove", "green", "Rishi");
+            Calendar.eventList.Add(testCourse);
+            Assert.True(Calendar.eventList.Contains(testCourse));
+            Calendar.eventList.Remove(testCourse);
+            Assert.False(Calendar.eventList.Contains(testCourse));
+        }
+    }
+}
