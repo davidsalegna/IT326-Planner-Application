5f6d23aa87515bd2407a95930207b2b610fed525
dasalegna <98178004+davidsalegna@users.noreply.github.com>
2022-12-02 09:14:52 -0600

    additional unit testing

---
+++ PlannerTesting/GetCourseList.cs
@@ -0,0 +1,20 @@
+﻿using Bunit;
+using IT_326___Planner_Application.Models;
+using IT_326___Planner_Application.Pages;
+using Xunit.Sdk;
+
+namespace PlannerTesting
+{
+    public class GetCourseList : TestContext
+    {
+        [Fact]
+        public void EditEventAttributes()
+        {
+            IList<Course> courseListTest = Calendar.getCourseList();
+            foreach (var i in courseListTest)
+            {
+                Assert.Equal(typeof(Course), i.GetType());
+            }
+        }
+    }
+}
